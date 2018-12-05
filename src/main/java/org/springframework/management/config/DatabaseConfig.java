11b034074d8c47409a944b99057f7a2bfd2729f9
allen_yang <allen_yang@kingdee.com>
2018-12-05 19:21:50 +0800

    bug fix

--- src/main/java/org/springframework/management/config/DatabaseConfig.java
+++ src/main/java/org/springframework/management/config/DatabaseConfig.java
@@ -5,12 +5,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
     @Qualifier("managementDS")
+    @Primary
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource") // application.properteis中对应属性的前缀
     public DataSource managementDS() {
